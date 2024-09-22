835df061e21e742ddf36dbd25482c1c61819c3dd
akib1437 <akibtanzim8@gmail.com>
2024-09-22 22:57:57 +0600

    added the partially add money section

--- js/home.js
+++ js/home.js
@@ -1,1 +1,16 @@
-console.log('home add');
+// add money to the account
+
+// step-1: add an event handler to the add money button inside the form
+document.getElementById('btn-add-money')
+    .addEventListener('click', function (event) {
+        // prevent page reload after form submit
+        event.preventDefault();
+
+        // step-2: get money oto be added to the account balance
+        const addMoneyInput = document.getElementById('input-add-money').value;
+        console.log(addMoneyInput);
+
+        // step-3: get the pin number
+        const pinNumberInput = document.getElementById('input-pin-number').value;
+        console.log(pinNumberInput);
+})
