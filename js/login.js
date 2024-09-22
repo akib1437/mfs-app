dd71e20603fc697a4deee566db81a18fcd57df0c
akib1437 <akibtanzim8@gmail.com>
2024-09-22 20:41:14 +0600

    login js done to homepage

--- js/login.js
+++ js/login.js
@@ -2,12 +2,21 @@
 
 // step-1: set event handler
 document.getElementById('btn-login')
-    .addEventListener('click', function(event){
-        // step-2: prevent default behavior(prevent reloading browser)
+    .addEventListener('click', function (event) {
+// step-2: prevent default behavior(prevent reloading browser)
         event.preventDefault();
         // console.log("login buttn clicked");
-        // step-3: get the phone number
-        const phoneNumber= document.getElementById('phone-number').value;
-        console.log(phoneNumber);
+// step-3: get the phone number
+        const phoneNumber = document.getElementById('phone-number').value;
+        const pinNumber = document.getElementById('pin-number').value;
+        console.log(phoneNumber, pinNumber);
 
-})
+// step-4: validate the pin
+        if (phoneNumber === '5' && pinNumber === '1234') {
+            console.log('you are logged in');
+            window.location.href= '/home.html';
+        }
+        else {
+            alert('wrong phone number or pin')
+        }
+    })
